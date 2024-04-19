9c2c25eea6b3a3ff479bc049e8dd2cdf2f481358
ejgalvis <158492223+ejgalvis@users.noreply.github.com>
2024-04-19 16:23:52 -0700

    Update content for DC5

--- Digital_Chalkboards/Week_5/js/DC_5.js
+++ Digital_Chalkboards/Week_5/js/DC_5.js
@@ -1,53 +1,48 @@
 // Sets up function for evaluating the trivia question on HTML page
 function checkTriviaAnswer() {
     const answer = document.getElementById("trivia-answer").value.trim();
-    const responseElement= document.getElementById("trivia-response");
     const correctAnswer = "Paris";
-    
+
     if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
-        console.log(responseElement.textContent = 'Correct! You guessed ${answer}.');
+        console.log(document.getElementById("trivia-response").innerText = `Correct! You guessed ${answer}.`);
     } else {
-        console.log(responseElement.textContent = 'Sorry, ${answer} is incorrect. Try again.');
+        console.log(document.getElementById("trivia-response").innerText = `Sorry, ${answer} is incorrect. Try again.`);
         }
-    // answer.textContent = placeholder;
-    // console.log("After submission:", answer.textContent);
-}
+} 
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const triviaInput = document.getElementById("trivia-answer");
-
-//     triviaInput.addEventListener('keypress', function (event) {
-//     if (event.key === 'Enter') {
-//         event.preventDefault();
-//         checkTrivia();
-//         }
-//     });
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const answer = document.querySelector("input#trivia-answer");
+    answer.addEventListener("keypress", function(event) {
+        if (event.key === "Enter"){
+            event.preventDefault();
+            checkTriviaAnswer();
+        }
+    });
+    const numInput = document.querySelector("input#number-input");
+    numInput.addEventListener("keypress", function(event){
+        if (event.key === "Enter") {
+            event.preventDefault();
+            checkNumber();
+        }
+    });
+});
 
 // Sets up function for evaluating the odd/even status from integer input on HTML page
 function checkNumber() {
     let numInput = document.getElementById("number-input").value;
-    const responseElement = document.getElementById("number-response");
+
+    const not_num = new RegExp("[A-Za-z./]");
+    if (not_num.test(numInput)){
+        console.log(document.getElementById("number-response").innerText = "Please enter a valid 5-digit integer.");
+    } else {
+
     let num = parseInt(numInput, 10);
 
-    if (num <= 9999 | num >= 100000 | !(Number.isInteger(num))) {
-        console.log(responseElement.textContent = "Please enter a valid 5-digit integer.");
-        return;
-    }
-
-    if (num % 2 === 0) {
-        console.log(responseElement.textContent = '$(num) is an even number');
+    if (!isNaN(num) && num >= 10000 && num <= 99999){
+        const isEven = num % 2 === 0;
+        console.log(document.getElementById("number-response").innerText = `The number ${num} is ${isEven ? "even": "odd"}.`);
     } else {
-        console.log(responseElement.textContent = '$(num) is an odd number');
+        console.log(document.getElementById("number-response").innerText = "Please enter a valid 5-digit integer.");
     }
 }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const triviaInput = document.getElementById("number-input");
-//     triviaInput.addEventListener('keypress', function (event) {
-//     if (event.key === 'Enter') {
-//         event.preventDefault();
-//         checkTrivia();
-//         }
-//     });
-// });
+}
