652465d4248c6414e325d26bba8ac0d7bfbed7b7
ejgalvis <158492223+ejgalvis@users.noreply.github.com>
2024-04-22 22:38:02 -0500

    Updating week5 TP and navbar

---
+++ Technical_Presentations/Week_5/js/TP5_js.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var instruments = document.getElementsByClassName('instrument');
+    const instrumentObject = new Object();
+
+    for(element of instruments) {
+      // Create properties like "instrument1: 'Drum'"
+      instrumentObject[element.nextElementSibling.id] = element.innerHTML
+    }
+    for (const property in instrumentObject){
+        document.getElementById(property).addEventListener('click', () => {
+          playAnimal(instrumentObject[property]);
+        })
+      }
+
+      for (const property in instrumentObject){
+        document.getElementById(property).addEventListener('mouseover', mouseOverHandler)
+        document.getElementById(property).addEventListener('mouseout', mouseOutHandler)
+      }
+      
+      function mouseOverHandler() {
+        this.style.border = '3px solid red';
+      }
+      function mouseOutHandler() {
+        this.style.border = '0px';
+      }
+    });
+
+function playAnimal(instrumentSound) {
+  var sound = new Howl({
+    src: [`audio/${instrumentSound}.mp3`, `audio/${instrumentSound}.wbm`],
+    sprite: {
+      Drum: [0, 3000],
+      Guitar: [0, 3000],
+      Organ: [0, 3000],
+      Human: [0, 3000],
+    }
+  });
+  sound.play(instrumentSound);
+}
