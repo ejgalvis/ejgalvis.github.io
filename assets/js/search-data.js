31cab7917d205f183d95ac1381d2c57242b76508
ejgalvis <158492223+ejgalvis@users.noreply.github.com>
2025-04-16 01:30:01 +0000

    Deploying to gh-pages from @ ejgalvis/ejgalvis.github.io@10cc4aa9e71c404b0a36b611ad064bf2589f84ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,22 +24,22 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          title: "Repositories",
+          description: "Check out some of the projects and tutorials I&#39;ve developed below.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
